b5af1788ee096bf8edb3a2c288452d3d6c1ca735
Kingdon Barrett <kbarret8@nd.edu>
2016-09-30 09:14:33 -0400

    ReferenceError: display_employee is not defined

--- app/assets/javascripts/nd_employee_lookup/employee_lookup.js
+++ app/assets/javascripts/nd_employee_lookup/employee_lookup.js
@@ -1,3 +1,68 @@
+function set_employee_input ( emp_sel_box) {
+  var emp_array = {};
+
+  var last_span = $('#'+emp_sel_box).find('span.emp_sel_last');
+  emp_array.last_name = $('#'+emp_sel_box).find('span.emp_sel_last').text();
+  emp_array.first_name = $('#'+emp_sel_box).find('span.emp_sel_first').text();
+  emp_array.pidm = $('#'+emp_sel_box).find('span.emp_sel_pidm').text();
+  emp_array.net_id = $('#'+emp_sel_box).find('span.emp_sel_net_id').text();
+  emp_array.home_orgn = $('#'+emp_sel_box).find('span.emp_sel_home_org').text();
+  emp_array.home_orgn_desc = $('#'+emp_sel_box).find('span.emp_sel_home_org_desc').text();
+  emp_array.nd_id = $('#'+emp_sel_box).find('span.emp_sel_nd_id').text();
+  emp_array.primary_title = $('#'+emp_sel_box).find('span.emp_sel_title').text();
+  return emp_array;
+}
+function employee_section(mode,input) {
+  if (mode == "display") {
+    if (input !== undefined) {
+      $('#employee_name').html(input.last_name + ", " + input.first_name);
+      $('#employee_net_id').html(input.net_id);
+      $('#employee_nd_id').html(input.nd_id);
+      $('#employee_home_org').html(input.home_orgn + ", " + input.home_orgn_desc);
+      $('#employee_title').html(input.primary_title);
+      $('#pidm_input').val(input.pidm);
+      $('#nd_id_input').val(input.nd_id);
+      $('#net_id_input').val(input.net_id);
+      $('#last_name_input').val(input.last_name);
+      $('#first_name_input').val(input.first_name);
+      $('#home_orgn_input').val(input.home_orgn);
+      $('#home_orgn_desc_input').val(input.home_orgn_desc);
+      $('#primary_title_input').val(input.primary_title);
+    }
+    $('#employee_edit').hide();
+    $('#employee_row').css("margin-bottom", "0px");
+    $('#edit_employee_button').show();
+    $('#close_edit_employee_button').hide();
+    $('#employee_display').show();
+  }
+  if (mode == "edit") {
+    $('#employee_display').hide();
+    $('#edit_employee_button').hide();
+    $('#close_edit_employee_button').show();
+    $('#employee_edit').show();
+    $('#employee_row').css("margin-bottom", "100px");
+
+  }
+}
+function display_employee(input) {
+  $('#employee_not_found').hide();
+  $('#employee_invalid_entries').hide();
+  employee_section("display",input);
+  $('#job_id_input').val("");
+  $('#pict_code_input').val("");
+  // $('#lu_date_edit').show();
+  // $('#lu_date_display').hide();
+  $('#lu_date_row').show();
+  $('#display_job_lookup_errors').hide();
+  $('#display_pay_lookup_errors').hide();
+  $('#employee_edit').hide();
+  $('#employee_row').css("margin-bottom", "0px");
+  $('#edit_employee_button').show();
+  $('#close_edit_employee_button').hide();
+  $('#employee_display').show();
+  $('#s_start_date').focus();
+  // clear_job_data();
+}
 function do_alert(msg) {
   $('#alert_text').text(msg);
   $('#alert_box_div').show();
@@ -32,7 +97,7 @@ function doActionOnReturn( e, field_id) {
       case "s_last_name":
       case "s_first_name": emp_lookup(); break;
       case "s_end_date": if ($('#b_find_jobs').length > 0) $('#b_find_jobs').click();
-                         else if ($('#b_find_pay').length > 0) $('#b_find_pay').click(); break;
+                           else if ($('#b_find_pay').length > 0) $('#b_find_pay').click(); break;
       case "f_notification_input_last":
       case "f_notification_input_first":
       case "f_notification_input_net_id": find_notification_employees(); break;
@@ -50,11 +115,11 @@ function enable_notifications() {
     find_notification_employees();  });
 }
 function disableConfirmExit() {
-    window.onbeforeunload = null;
-    $('.top-bar-section >ul>li>a').on("click", function(e) {
-      return true;
+  window.onbeforeunload = null;
+  $('.top-bar-section >ul>li>a').on("click", function(e) {
+    return true;
 
-    });
+  });
 }
 
 function emp_lookup() {
@@ -71,19 +136,19 @@ function emp_lookup() {
   else {
     if (netid != "")  lookup_url = lookup_url + '/' + netid;
     else  {
-        $('#employee_invalid_entries').show();
-        return;
+      $('#employee_invalid_entries').show();
+      return;
     }
   }
-//  lookup_url = lookup_url + ".json";
+  //  lookup_url = lookup_url + ".json";
   $('#find_employee_processing').addClass("ajax-processing");
   $.ajax({ url: lookup_url,
     contentType: "json",
     dataType: "json",
     cache: false,
     error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    $('#find_employee_processing').removeClass("ajax-processing");
-                    do_alert("An error has occurred while attempting to retrieve employee data.  Please confirm that all necessary web services are running. (" + errorThrown + ")");                 },
+      $('#find_employee_processing').removeClass("ajax-processing");
+      do_alert("An error has occurred while attempting to retrieve employee data.  Please confirm that all necessary web services are running. (" + errorThrown + ")");                 },
     success: function (data, status, xhr) {
       if (data.length == 0) {
         $('#employee_not_found').show();
@@ -108,7 +173,7 @@ function emp_lookup() {
           });
         }
       }
-      $('#find_employee_processing').removeClass("ajax-processing");
+  $('#find_employee_processing').removeClass("ajax-processing");
     }
   });
 
