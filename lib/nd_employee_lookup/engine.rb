0a079c5c6fd707d72b9da9ac9ddf91ae73a3bcdd
Kingdon Barrett <kbarret8@nd.edu>
2016-09-30 11:37:16 -0400

    these assets aren't included automatically
    when NdEmployeeLookup::ApplicationController < ::ApplicationController

--- lib/nd_employee_lookup/engine.rb
+++ lib/nd_employee_lookup/engine.rb
@@ -1,5 +1,8 @@
 module NdEmployeeLookup
   class Engine < ::Rails::Engine
+    config.assets.paths << File.expand_path("../../assets/stylesheets", __FILE__)
+    config.assets.paths << File.expand_path("../../assets/javascripts", __FILE__)
+    config.assets.precompile += %w( employee_lookup.js employee_lookup.css )
     config.autoload_paths += %W(#{config.root}/lib)
     isolate_namespace NdEmployeeLookup
   end
