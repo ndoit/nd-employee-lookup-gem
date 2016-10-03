b56b6ee0ac327172bf5477c435ff2614270c9c16
Kingdon Barrett <kbarret8@nd.edu>
2016-10-03 11:36:10 -0400

    rails g rspec:install

--- lib/nd_employee_lookup/engine.rb
+++ lib/nd_employee_lookup/engine.rb
@@ -2,5 +2,10 @@ module NdEmployeeLookup
   class Engine < ::Rails::Engine
     config.autoload_paths += %W(#{config.root}/lib)
     isolate_namespace NdEmployeeLookup
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
