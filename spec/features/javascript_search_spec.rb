11d0c58f067f1093ba243e2bb31dac21e933381f
Kingdon Barrett <kbarret8@nd.edu>
2016-10-06 12:02:22 -0400

    headless javascript testing!11

--- spec/features/javascript_search_spec.rb
+++ spec/features/javascript_search_spec.rb
@@ -1,11 +1,28 @@
 require 'support/fake_hr_py'
 
 feature 'JavaScript search controller' do
+  ## FIXME: We should really check for JS errors after every JS test
   it 'should not have JavaScript errors', js: true do
     visit '/employee-lookup/search'
     expect(page).not_to have_errors
   end
-  it 'should ...' do
-    pending
+  it 'should answer a specific query with a single result', js: true do
+    visit '/employee-lookup/search'
+    fill_in 's_last_name', with: 'Meyer'
+    fill_in 's_first_name', with: 'Teresa'
+    find('#b_find_employee').click
+
+    name = page.find 'span#employee_name'
+    expect(name.text).to match(/^Meyer, Teresa$/)
+  end
+  it 'should answer a less specific query with a list', js: true do
+    visit '/employee-lookup/search'
+    fill_in 's_last_name', with: 'Meyer'
+    fill_in 's_first_name', with: 'T'
+    find('#b_find_employee').click
+
+    results = page.find 'div#select_employee_list'
+    expect(results).to have_selector('.emp_sel_box', visible: true)
+    expect(page.all('.emp_sel_box').count).to eq 2
   end
 end
