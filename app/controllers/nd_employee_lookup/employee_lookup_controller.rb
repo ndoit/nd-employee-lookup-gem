4d67c373981bad0e86b1551143924a03842cd766
Kingdon Barrett <kbarret8@nd.edu>
2016-10-12 16:00:11 -0400

    swap fat controller methods for HrpyEmployeePerson

--- app/controllers/nd_employee_lookup/employee_lookup_controller.rb
+++ app/controllers/nd_employee_lookup/employee_lookup_controller.rb
@@ -17,13 +17,8 @@ module NdEmployeeLookup
     end
 
     def search
-      clean_params = sanitize_params(params)
-      lookup_url = "#{ENV['HRPY_API_BASE']}/employee/v1"
-      lookup_url += query_string(clean_params)
-      lookup_url += "?api_key=#{ENV['HRPY_API_KEY']}"
-
-      url_open = open(lookup_url)
-      search_results = JSON.parse(url_open.read)
+      json = HrpyEmployeePerson.search(params)
+      search_results = JSON.parse(json)
       if search_results.empty?
         render :json => JSON.parse('[{ "Employee": "None", "employee_title": "No matching records"}]')
       else
@@ -59,61 +54,6 @@ module NdEmployeeLookup
       else
         raise e
       end
-    end
-
-    private
-
-    def sanitize_params(params)
-      cparams = {}
-      if params.key?(:status)
-        if params[:status] == "active"
-          cparams[:status] = "active"
-        else
-          raise InvalidParams
-        end
-      end
-      if params.key?(:last_name)
-        if params[:last_name].length <= 60
-          cparams[:last_name] = params[:last_name]
-        else
-          raise InvalidParams
-        end
-      end
-      if params.key?(:first_name)
-        if params[:first_name].length <= 60
-          cparams[:first_name] = params[:first_name]
-        else
-          raise InvalidParams
-        end
-      end
-      if params.key?(:search_string)
-        if params[:search_string] =~ /[a-zA-Z0-9]/
-          cparams[:search_string] = params[:search_string]
-        else
-          raise InvalidParams
-        end
-      end
-      return cparams
-    rescue => e
-      raise e
-    end
-
-    def query_string(params)
-      qs = ""
-      if params.key?(:status)
-        qs += "/" + params[:status]
-      end
-
-      if params.key?(:last_name)
-        qs += "/l/" + URI.encode(params[:last_name])
-      end
-      if params.key?(:first_name)
-        qs += "/" + URI.encode(params[:first_name])
-      end
-      if params.key?(:search_string)
-        qs += "/" + URI.encode(params[:search_string])
-      end
-      qs
     end
   end
 end
