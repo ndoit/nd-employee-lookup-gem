d3dac67428bf4d4dd54c6cec24bbb6511fd9c7a3
Kingdon Barrett <kbarret8@nd.edu>
2016-10-13 09:19:19 -0400

    these specs are not pending anymore

--- spec/views/new.html.erb_spec.rb
+++ spec/views/new.html.erb_spec.rb
@@ -2,7 +2,6 @@ describe "nd_employee_lookup/employee_lookup/new.html.erb" do
   it "renders the employee_search partial" do
     render
     expect(view).to render_template(:new)
-    pending "the partial is not yet in place"
-    expect(view).to render_template(partial: "_employee_search")
+    expect(view).to render_template(partial: "_search")
   end
 end
