2852bf4e79f79962249fe7eabc0995b1a816dc7b
Kingdon Barrett <kbarret8@nd.edu>
2016-10-12 22:33:00 -0400

    mark one other test as pending

--- spec/views/new.html.erb_spec.rb
+++ spec/views/new.html.erb_spec.rb
@@ -2,6 +2,7 @@ describe "nd_employee_lookup/employee_lookup/new.html.erb" do
   it "renders the employee_search partial" do
     render
     expect(view).to render_template(:new)
+    pending "the partial is not yet in place"
     expect(view).to render_template(partial: "_employee_search")
   end
 end
