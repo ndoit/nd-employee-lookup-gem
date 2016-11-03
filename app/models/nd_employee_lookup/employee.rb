c226dcfdd74251b50dd337d186a36a3ccd8277d3
Kingdon Barrett <kbarret8@nd.edu>
2016-11-03 14:55:31 -0400

    wishful thinking

--- app/models/nd_employee_lookup/employee.rb
+++ app/models/nd_employee_lookup/employee.rb
@@ -1,7 +1,6 @@
 module NdEmployeeLookup
   class Employee < ::ActiveRecord::Base
-    # NdJobLookup:: module will monkey-patch these into Employee
-    # has_many :employee_jobs
-    # accepts_nested_attributes_for :employee_jobs
+    has_many :employee_jobs, class_name: 'NdJobLookup::EmployeeJob'
+    accepts_nested_attributes_for :employee_jobs
   end
 end
