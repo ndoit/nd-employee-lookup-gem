391ef0c5a3890836b5514017fbe23e48f602fbab
Kingdon Barrett <kbarret8@nd.edu>
2016-11-10 16:30:53 -0500

    convenience methods for extension of models
    and controllers

--- lib/nd_employee_lookup.rb
+++ lib/nd_employee_lookup.rb
@@ -3,4 +3,15 @@ require "jquery-rails"
 require "foundation-rails"
 
 module NdEmployeeLookup
+  def self.root
+    File.expand_path(File.dirname(File.dirname(__FILE__)))
+  end
+
+  def self.models_dir
+    "#{root}/app/models/nd_employee_lookup"
+  end
+
+  def self.controllers_dir
+    "#{root}/app/controllers/nd_employee_lookup"
+  end
 end
