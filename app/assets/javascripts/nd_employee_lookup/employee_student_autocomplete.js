03199acf898370e85334feaed4f2c2610d016591
Kingdon Barrett <kingdon.b@nd.edu>
2018-11-16 13:10:07 -0500

    personLookup -> employeeStudentLookup
    s/person/employee_student/g (mostly this)

---
+++ app/assets/javascripts/nd_employee_lookup/employee_student_autocomplete.js
@@ -0,0 +1,153 @@
+/* Add the class employee_student_id_input to your input field
+ * Call employeeStudentLookup.set_employee_student_id_autocomplete in document ready
+ * Select and change will call a function named:
+ * select_employee_student_id_input(search_field, ndid) if it is available
+ */
+
+var employeeStudentLookup = (
+  () => {
+
+    var ajax_params_base = (search_term, error, success) => {
+      return {
+        url: "/employee-lookup/employee-student-basic/" + encodeURIComponent(search_term),
+        dataType: "json",
+        data: { },
+        error: error,
+        success: success
+      }
+    }
+
+    var get_employee_student_from_id = (id) => {
+      $.ajax(
+        employeeStudentLookup.ajax_params_base(id, errorFunction, successFunction)
+      );
+
+      function errorFunction(XMLHttpRequest, textStatus, errorThrown) {
+        if (errorThrown != 'Not Found')
+          alert('An error occurred while searching NDIDs.  ' +
+            'Verify the HR/PY API is running [' + errorThrown + ']');
+        get_employee_student_from_id_employee_student_found = {error: errorThrown};
+      }
+
+      function successFunction(data) {
+        switch (data.length) {
+          case 1:
+            get_employee_student_from_id_employee_student_found = data[0];
+            break;
+          case 0:
+            get_employee_student_from_id_employee_student_found = {
+              error: 'Person could not be found using provided ID.'}
+            break;
+          default:
+            var employee_student_found = false;
+            for(i=0; i<data.length; i++) {
+              if (data[i].ndid == id || data[i].net_id == id) {
+                get_employee_student_from_id_employee_student_found = data[i];
+                employee_student_found = true;
+                break;
+              }
+            }
+            if (!employee_student_found)
+              get_employee_student_from_id_employee_student_found = {
+                error: 'Person could not be found using provided ID.'};
+        }
+      }
+    }
+
+    var set_employee_student_id_autocomplete = (active = '') => {
+
+      var lookup_employee_student = (request, response) => {
+        var ajax_params =
+          ajax_params_base( request.term,
+            lookup_employee_student_error_function, lookup_employee_student_success_function)
+
+        $.ajax(ajax_params);
+
+        function lookup_employee_student_error_function(XMLHttpRequest, textStatus, errorThrown) {
+          lookup_employee_student_error_base(XMLHttpRequest, textStatus, errorThrown, response)
+        }
+        function lookup_employee_student_success_function(data) {
+          lookup_employee_student_success_base(data, response)
+        }
+      }
+
+      function select_employee_student(event, ui) {
+        if (ui.item.value.trim() == '') {
+          event.preventDefault();
+          return;
+        }
+        get_employee_student_from_id(ui.item.value);
+        $(this).val(ui.item.value);
+        $(this).trigger('change');
+        $(this).trigger('blur');
+      }
+      function employee_student_input_change_function(event, ui) {
+        if (typeof select_employee_student_id_input == 'function') {
+          select_employee_student_id_input(this, this.value);
+        }
+      }
+
+      $(".employee_student_id_input").autocomplete({
+        source: lookup_employee_student,
+        minLength: 3,
+        delay: 300,
+        autoFocus: true,
+        select: select_employee_student,
+        change: employee_student_input_change_function
+      });
+
+    }
+
+
+    var lookup_employee_student_error_base =
+      (XMLHttpRequest, textStatus, errorThrown, response) => {
+        if (errorThrown != 'Not Found')
+          alert('An error occurred while searching NDIDs.  ' +
+            'Verify the HR/PY API is running [' + errorThrown + ']');
+        response([]);
+      }
+
+    var lookup_employee_student_success_base =
+      (data, response) => {
+        if(!data.length){
+          var result = [
+            {
+              label: 'No matches found',
+              value: ' '
+            }
+          ];
+          response(result);
+        }
+        else{
+          response( $.map(data, format_employee_student_function));
+        }
+      }
+
+    var format_employee_student_function = (item) => {
+      var middle_initial = "";
+      var pretty_employee_student_id = item.employee_student_id;
+      if(typeof item.ndid !== "undefined") {
+        pretty_employee_student_id = item.ndid
+      }
+      if(typeof item.netid !== "undefined") {
+        pretty_employee_student_id = item.netid
+      }
+      if(typeof item.middle_initial !== "undefined") {
+        middle_initial = " " + item.middle_initial
+      }
+      return {
+        label: pretty_employee_student_id + " - " + item.first_name + middle_initial + " " + item.last_name,
+        value: item.employee_student_id
+      }
+    }
+
+    return {
+      get_employee_student_from_id: get_employee_student_from_id,
+      set_employee_student_id_autocomplete: set_employee_student_id_autocomplete,
+      ajax_params_base: ajax_params_base,
+    }
+
+  }
+)()
+
+var get_employee_student_from_id_employee_student_found = Object.new;
